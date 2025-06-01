fca80b5758870f1fc7855eb8e041916c13f012fe
HabeebFF <habeebmuftau05@gmail.com>
2025-06-01 11:16:47 +0100

    CHANGED FROM LOCALHOST TO NORMAL URL

--- vent_auth/views.py
+++ vent_auth/views.py
@@ -258,7 +258,7 @@ def signup(request):
     # Generate verification link
     token = default_token_generator.make_token(user)
     uid = urlsafe_base64_encode(force_bytes(user.pk))
-    FRONTEND_VERIFY_URL = f"{FRONTEND_LOCALHOST}/email-verified"  # Or from settings
+    FRONTEND_VERIFY_URL = f"{FRONTEND_URL}/email-verified"  # Or from settings
     verification_link = f"{FRONTEND_VERIFY_URL}/{uid}/{token}"
 
     subject = 'Verify Your Email'
@@ -1905,7 +1905,7 @@ def resend_link(request):
     token = default_token_generator.make_token(user)
     uid = urlsafe_base64_encode(force_bytes(user.pk))
 
-    FRONTEND_VERIFY_URL = f"{FRONTEND_LOCALHOST}/email-verified"  # Use your actual frontend URL
+    FRONTEND_VERIFY_URL = f"{FRONTEND_URL}/email-verified"  # Use your actual frontend URL
     verification_link = f"{FRONTEND_VERIFY_URL}/{uid}/{token}"
 
     subject = 'Resend: Verify Your Email'
@@ -1975,7 +1975,7 @@ def google_callback(request):
     id_token_str = token_response_data['id_token']
 
     # Redirect user to frontend with id_token
-    frontend_redirect_url = f"{FRONTEND_LOCALHOST}/verify-google-token?id_token={id_token_str}"
+    frontend_redirect_url = f"{FRONTEND_URL}/verify-google-token?id_token={id_token_str}"
 
     return redirect(frontend_redirect_url)
 
